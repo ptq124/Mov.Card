51dd2856acecac9f3ec6f92850eb5bf0dd1c8504
박태규 Park Tae Kyu <ptq124@naver.com>
2023-12-08 03:06:12 +0900

    chore: 디자인 수정

--- src/components/navbars/Nav.jsx
+++ src/components/navbars/Nav.jsx
@@ -5,13 +5,12 @@ import ResultNav from './ResultNav'
 import SelectNav from './SelectNav'
 import StartNav from './StartNav'
 const Header = styled.header`
-  height: 15%;
   width: 80%;
-  margin: auto;
   display: flex;
   @media screen and (max-width: 540px) {
     height: 7%;
   }
+  margin: 0 auto;
 `
 export default function Nav() {
   const { pathname } = useLocation()
