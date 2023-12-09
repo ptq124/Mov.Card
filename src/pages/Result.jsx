051edae1b376abf5b8f667a4ac12e19144b6dffa
박태규 Park Tae Kyu <ptq124@naver.com>
2023-12-09 21:57:41 +0900

    refactor: 결과페이지 이미지 프리로딩 적용

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getResultData } from '../apis/result'
 import styled from 'styled-components'
@@ -8,45 +8,31 @@ import ResultInfo from '../components/ui/ResultInfo'
 import MovieChange from '../components/ui/MovieChange'
 import NavArrow from '../components/ui/NavArrow'
 import Nav from '../components/navbars/Nav'
+import StartLoading from '../components/ui/StartLoading'
 
-const ResultPage = styled.section`
-  height: 65%;
-  width: 70%;
-  margin: auto;
-  display: flex;
-  align-items: flex-end;
-
-  @media screen and (max-width: 1600px) {
-    width: 75%;
-  }
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    height: 85%;
-  }
-`
-
-const ChangeBtn = styled.div`
-  height: 60%;
-  width: 15%;
-
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
-`
-const Arrow = styled.div`
-  position: absolute;
-  top: 30px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  @media screen and (min-width: 800px) {
-    display: none;
-  }
-`
 export default function Result() {
   const { key } = useParams()
   const [data, setData] = useState([])
   const [background, setBg] = useState([])
+  const [imgLoading, setImgLoading] = useState(true)
+
+  const preLoadImgs = useCallback(() => {
+    return Promise.all(
+      data.forEach((item) => {
+        const img = new Image()
+        img.src = `/imgs/background/${item.image[0].background}`
+      })
+    )
+  }, [data])
+
+  const preLoadPoster = useCallback(() => {
+    return Promise.all(
+      data.forEach((item) => {
+        const img = new Image()
+        img.src = `/imgs/poster/${item.image[0].poster}`
+      })
+    )
+  }, [data])
 
   useEffect(() => {
     getResultData(key) //
@@ -55,6 +41,14 @@ export default function Result() {
         setBg([...data.map((d) => d.image[0].background)])
       }) //
   }, [key])
+
+  useEffect(() => {
+    preLoadImgs()
+    preLoadPoster()
+    setTimeout(() => {
+      setImgLoading(false)
+    }, 2000)
+  }, [preLoadImgs, preLoadPoster])
 
   useEffect(() => {
     const main = document.getElementsByTagName('main')
@@ -83,7 +77,9 @@ export default function Result() {
     setBg([...background])
   }
 
-  return (
+  return imgLoading ? (
+    <StartLoading />
+  ) : (
     <>
       <Arrow>
         <NavArrow 결과데이터조작={결과데이터조작} />
@@ -101,3 +97,39 @@ export default function Result() {
     </>
   )
 }
+
+const ResultPage = styled.section`
+  height: 65%;
+  width: 70%;
+  margin: auto;
+  display: flex;
+  align-items: flex-end;
+
+  @media screen and (max-width: 1600px) {
+    width: 75%;
+  }
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    height: 85%;
+  }
+`
+
+const ChangeBtn = styled.div`
+  height: 60%;
+  width: 15%;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+const Arrow = styled.div`
+  position: absolute;
+  top: 30px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  @media screen and (min-width: 800px) {
+    display: none;
+  }
+`
