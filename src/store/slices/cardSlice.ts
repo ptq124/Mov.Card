d23c49b59743d90dd9d0f66ce7a686a9d71bbc48
박태규 Park Tae Kyu <ptq124@naver.com>
2024-04-19 11:26:45 +0900

    refactor: context에서 rtk 변환

--- src/store/slices/cardSlice.ts
+++ src/store/slices/cardSlice.ts
@@ -12,7 +12,9 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   cardData: [],
+  selectData: [],
   stage: 0,
+  id: '',
 }
 
 const cardSlice = createSlice({
@@ -23,8 +25,21 @@ const cardSlice = createSlice({
       state.cardData = action.payload
       state.stage = 0
     },
+    selectCard: (state, action): void => {
+      state.selectData.push(action.payload)
+    },
+    popCard: (state): void => {
+      state.selectData.pop()
+    },
+    changeStage: (state, action): void => {
+      if (0 <= state.stage && state.stage < 3) {
+        state.cardData[state.stage].map((item) => (item.display = false))
+        action.payload === 'up' ? state.stage++ : state.stage--
+        state.cardData[state.stage].map((item) => (item.display = true))
+      }
+    },
   },
 })
 
-export const { initData } = cardSlice.actions
+export const { initData, selectCard, popCard, changeStage } = cardSlice.actions
 export default cardSlice.reducer
